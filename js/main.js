00bf8e831a3c81fa7f2cab6514d4ef57fa9fda3b
Marina Kudryashova <milenacheb@mail.ru>
2022-10-31 11:11:23 +0300

    add media section hero

--- js/main.js
+++ js/main.js
@@ -1,53 +1,45 @@
-// иницилизация yandex карты
-let center = [56.143012068597976,47.191241500000004];
+// открыть\закрыть бургер-меню
+let burger = document.querySelector('.btn-burger');
+let menu = document.querySelector('.menu__list');
+let menuLink = document.querySelectorAll('.menu__link');
+let body = document.body;
 
-function init() {
-    let map = new ymaps.Map('contacts__map', {
-        center: center,
-        zoom: 17,
-        suppressMapOpenBlock: true,
-        // controls: ['routePanelControl'],
-    });
+burger.addEventListener('click', function() {
+  burger.classList.toggle('btn-burger-active');
+  menu.classList.toggle('menu__list-active');
+  body.classList.toggle('stop-scroll');
+  if (burger.getAttribute('aria-label') === 'Открыть меню') {
+    burger.setAttribute("aria-label", 'Закрыть меню');
+  } else {
+    burger.setAttribute("aria-label", 'Открыть меню');
+  }
+});
+menuLink.forEach(function(e) {
+  e.addEventListener('click', function() {
+    burger.classList.remove('btn-burger-active');
+    burger.setAttribute("aria-label", 'Открыть меню');
+    menu.classList.remove('menu__list-active');
+    body.classList.remove('stop-scroll');
+  });
+});
+document.addEventListener('click',
+function(el) {
+  let target = el.target;
+  if(!target.closest('.header__burger')) {
+    burger.classList.remove('btn-burger-active');
+    burger.setAttribute("aria-label", 'Открыть меню');
+    menu.classList.remove('menu__list-active');
+    body.classList.remove('stop-scroll');
+  }
 
-    let placemark = new ymaps.Placemark(center, {}, {
-        // iconLayout: '',
-        // iconImageHref: '',
-        // iconImageSize: [],
-        // iconImageOffset: [],
-    });
+});
 
-    // let control = map.controls.get('routePanelControl');
-    // let city = 'Чебоксары';
-    // let addressOffice = 'ул. Мичмана Павлова, дом 8';
-
-    // control.routePanel.state.set({
-    //     type: 'masstransit',
-    //     fromEnabled: true,
-    //     toEnabled: true,
-    //     to: `${city}, ${addressOffice}`,
-    // });
-
-    map.controls.remove('geolocationControl'); // удаляем геолокацию
-    map.controls.remove('searchControl'); // удаляем поиск
-    map.controls.remove('trafficControl'); // удаляем контроль трафика
-    // map.controls.remove('typeSelector'); // удаляем тип
-    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-    // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-    map.controls.remove('rulerControl'); // удаляем контрол правил
-    // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
-    
-    map.geoObjects.add(placemark);// добавляем элемент на карту
-}
-
-ymaps.ready(init);
 
 // иницилизация modal-windows
 const btns = document.querySelectorAll('.btn-open');// массив кнопок
 const modalOverlay = document.querySelector('.modal__overlay');
 const modals = document.querySelectorAll('.modal');// массив окон
 const btnClose = document.querySelector('.btn-close');
-let body = document.body;
-
 
 btns.forEach(function(btn) {
     btn.addEventListener('click', function(e) {
@@ -149,6 +141,49 @@ const swiper = new Swiper('.swiper-slide', {
     },
   
   });
+
+  // иницилизация yandex карты
+let center = [56.143012068597976,47.191241500000004];
+
+function init() {
+    let map = new ymaps.Map('contacts__map', {
+        center: center,
+        zoom: 17,
+        suppressMapOpenBlock: true,
+        // controls: ['routePanelControl'],
+    });
+
+    let placemark = new ymaps.Placemark(center, {}, {
+        // iconLayout: '',
+        // iconImageHref: '',
+        // iconImageSize: [],
+        // iconImageOffset: [],
+    });
+
+    // let control = map.controls.get('routePanelControl');
+    // let city = 'Чебоксары';
+    // let addressOffice = 'ул. Мичмана Павлова, дом 8';
+
+    // control.routePanel.state.set({
+    //     type: 'masstransit',
+    //     fromEnabled: true,
+    //     toEnabled: true,
+    //     to: `${city}, ${addressOffice}`,
+    // });
+
+    // map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    // map.controls.remove('typeSelector'); // удаляем тип
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('rulerControl'); // удаляем контрол правил
+    map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+    
+    map.geoObjects.add(placemark);// добавляем элемент на карту
+}
+
+ymaps.ready(init);
 
 
 
